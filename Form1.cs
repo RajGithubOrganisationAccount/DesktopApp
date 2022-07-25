3ac2fb3be27330ed6c665c53f2eb2b0bb4a38731
Kumar <raj.kumar@siemens-healthineers.com>
2022-07-26 00:11:47 +0530

    pushing code in organisation

--- Form1.cs
+++ Form1.cs
@@ -99,6 +99,15 @@ namespace CraftDemonstration
 
 
         }
-     
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
